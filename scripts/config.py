a125c0340c3a59c0aa2cdbd7946dece288299274
choydarwin <choydarwin@gmail.com>
2025-02-13 12:45:01 -0600

    Refactored data extraction and transformation; identified issue in data_loading

--- scripts/config.py
+++ scripts/config.py
@@ -5,16 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Fetch AWS credentials from environment variables
-AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
-AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
-AWS_REGION = os.getenv("AWS_REGION")
+bucket_name = os.getenv("bucket_name")
+key = os.getenv("key")
 
 # Fetch RDS credentials from environment variables
 RDS_HOST = os.getenv("RDS_HOST")
 RDS_USER = os.getenv("RDS_USER")
 RDS_PASSWORD = os.getenv("RDS_PASSWORD")
 RDS_DATABASE = os.getenv("RDS_DATABASE")
-
-# Optional: You can check if the variables are loaded correctly
-print(AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_REGION)
-print(RDS_HOST, RDS_USER, RDS_PASSWORD, RDS_DATABASE)
